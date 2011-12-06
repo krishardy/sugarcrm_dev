ee1d49c5abc76feb58757901fa0da6c1774a833a
Dev <none@angel.rhs.com>
2011-12-05 18:18:05 -0700

    Adding opportunity to list of objects to QuickCreate from an email

---
+++ custom/modules/Emails/metadata/qcmodulesdefs.php
@@ -0,0 +1,10 @@
+<?php
+if (!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+$QCModules = array(
+    'Bugs',
+    'Cases',
+    'Contacts',
+    'Opportunities',
+    'Leads',
+    'Tasks'
+);
